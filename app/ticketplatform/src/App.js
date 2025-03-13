d7ea1f25d1d668270e48cf9afb82ab243fde5d5d
Georgi Stoev <georgi.stoev.2003@gmail.com>
2025-03-13 12:51:25 +0200

    App.js without routing through pages

--- app/ticketplatform/src/App.js
+++ app/ticketplatform/src/App.js
@@ -1,17 +1,15 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-import './pages/AboutUs'
 import AboutUs from './pages/AboutUs';
-import Navbar from './components/Navbar';
 import Homepage from './pages/Homepage';
 import Layout from './components/Layout';
 
 function App() {
   return (
-    <Layout>
-        <Homepage></Homepage>
-        <AboutUs />
-    </Layout>
+        <Layout>
+            <Homepage />
+            <AboutUs />
+        </Layout>
   );
 }
 
