4aa61fd701784b39f8d6fb439671ba4324d7446c
Georgi Stoev <georgi.stoev.2003@gmail.com>
2025-03-15 18:17:19 +0200

    Ready for production Navbar. Added FontAwesome link to the index.html to use icons from the site. Set the background of the website.

--- app/ticketplatform/src/components/Navbar.js
+++ app/ticketplatform/src/components/Navbar.js
@@ -1,42 +1,66 @@
 import React from 'react';
 import logo from '../logo.svg';
 import navbarStyles from "../styles/Navbar.module.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleUser, faTicket } from '@fortawesome/free-solid-svg-icons';
 
 const Navbar = () => {
     return (
         <header>
-                <div className={navbarStyles.navbar}>
-                    <img src={logo} className={navbarStyles['app-logo']}></img>
-                    <ul className={navbarStyles['navbar-logged-user']}> 
-                        <li className={`${navbarStyles['navbar-list']} ${navbarStyles['navbar-list:hover']}`}>
-                            <a className={`${navbarStyles['navbar-links']} ${navbarStyles['navbar-links:hover']}`} href="/">
-                            Home
+            <div className={navbarStyles.navbar}>
+
+                    <a className={navbarStyles['home-link-container']} href='/'>
+                        <FontAwesomeIcon className={navbarStyles['logo']} icon={faTicket} />
+                        <h1 className={navbarStyles['logo-text']}>TicketChain</h1>
+                    </a>
+
+                <div className={navbarStyles['navbar-links']}>
+
+                    <ul className={navbarStyles['navbar-ul']}>
+                        <li className={navbarStyles['navbar-list']}>
+
+                            <a className={navbarStyles['navbar-a']} href="/">
+                                Home
                             </a>
                         </li>
-                        <li className={`${navbarStyles['navbar-list']} ${navbarStyles['navbar-list:hover']}`}>
-                            <a className={`${navbarStyles['navbar-links']} ${navbarStyles['navbar-links:hover']}`} href="/events">
-                            Events
+
+                        <li className={navbarStyles['navbar-list']}>
+                            <a className={navbarStyles['navbar-a']} href="/events">
+                                Events
                             </a>
                         </li>
-                        <li className={`${navbarStyles['navbar-list']} ${navbarStyles['navbar-list:hover']}`}>
-                            <a className={`${navbarStyles['navbar-links']} ${navbarStyles['navbar-links:hover']}`} href="/about">
-                            AboutUs
+
+                        <li className={navbarStyles['navbar-list']}>
+                            <a className={navbarStyles['navbar-a']} href="/tickets">
+                                My Tickets
                             </a>
                         </li>
-                        <li className={`${navbarStyles['navbar-list']} ${navbarStyles['navbar-list:hover']}`}>
-                            <a className={`${navbarStyles['navbar-links']} ${navbarStyles['navbar-links:hover']}`} href="/tickets">
-                            My Tickets
+
+                        <li className={navbarStyles['navbar-list']}>
+                            <a className={navbarStyles['navbar-a']} href="/wallet">
+                                Connect Wallet
                             </a>
                         </li>
-                        <li className={`${navbarStyles['navbar-list']} ${navbarStyles['navbar-list:hover']}`}>
-                            <a className={`${navbarStyles['navbar-links']} ${navbarStyles['navbar-links:hover']}`} href="/wallet">
-                            Connect Wallet
+
+                        <li className={navbarStyles['navbar-list']}>
+                            <a className={navbarStyles['navbar-a']} href="/about">
+                                AboutUs
                             </a>
                         </li>
+
                     </ul>
+
                 </div>
+
+                <div className={navbarStyles['user-profile-container']}>
+                    <a className={navbarStyles['login-link']} href='/login'>
+                        <FontAwesomeIcon className={navbarStyles['login-image']} icon={faCircleUser} />
+                    </a>
+                </div>
+
+            </div>
         </header>
-            
+
     );
 }
 
