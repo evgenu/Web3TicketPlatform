926cbc986f58a61317ca77845424732a644a9c70
Georgi Stoev <georgi.stoev.2003@gmail.com>
2025-03-22 14:16:59 +0200

    Mobile navbar

--- app/ticketplatform/src/components/Navbar.js
+++ app/ticketplatform/src/components/Navbar.js
@@ -1,77 +1,87 @@
 import React, { useState } from 'react';
 import navbarStyles from "../styles/Navbar.module.css";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCircleUser, faTicket } from '@fortawesome/free-solid-svg-icons';
+import { faCircleUser, faTicket, faRectangleList } from '@fortawesome/free-solid-svg-icons';
 import { ethers } from 'ethers';
 
-
 import CONTRACT_ABI from '../constants/abis/TicketPlatform.json';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import { useContract } from '../hooks/contractHook';
 
 const Navbar = () => {
 
+    const [menuOpen, setMenuOpen] = useState(false);
     const { setContract } = useContract();
 
     const handleConnectWallet = async () => {
         if (window.ethereum != null) {
-            const currentprovider = new ethers.BrowserProvider(window.ethereum);
-            const signer = await currentprovider.getSigner();
+            const currentProvider = new ethers.BrowserProvider(window.ethereum);
+            const signer = await currentProvider.getSigner();
 
-            const currContract = new ethers.Contract("0x12dd4647bF90B39998bC4CB893FC1f96bE27ECc5", CONTRACT_ABI.abi, signer );
+            const currContract = new ethers.Contract("0x12dd4647bF90B39998bC4CB893FC1f96bE27ECc5", CONTRACT_ABI.abi, signer);
             setContract(currContract);
-           
+
         } else {
             alert('Please install MetaMask!');
         }
     }
 
+    const toggleMenu = () => {
+        // Flip the "menuOpen" boolean in state
+        setMenuOpen(prev => !prev);
+      };
+
     return (
-        <header>
-            <div className={navbarStyles.navbar}>
+        <header className={navbarStyles.navbar}>
 
-                <a className={navbarStyles['home-link-container']} href='/'>
-                    <FontAwesomeIcon className={navbarStyles['logo']} icon={faTicket} />
-                    <h1 className={navbarStyles['logo-text']}>TicketChain</h1>
-                </a>
+            <NavLink className={navbarStyles['home-link-container']} to="/">
+                <FontAwesomeIcon className={navbarStyles['logo']} icon={faTicket} />
+                <h1 className={navbarStyles['logo-text']}>TicketChain</h1>
+            </NavLink>
 
-                <nav className={navbarStyles['navbar-links']}>
+            <button
+                aria-expanded={menuOpen}
+                aria-controls="navbar-links"
+                className={navbarStyles['mobile-nav-toggle']}
+                onClick={toggleMenu}
+            >
+                <FontAwesomeIcon icon={faRectangleList} />
+            </button>
 
-                    <ul className={navbarStyles['navbar-ul']}>
-                        <li className={navbarStyles['navbar-list']}>
+            <nav>
+                <ul id="navbar-links" data-visible={menuOpen} className={navbarStyles['navbar-links']}>
+                    <li className={navbarStyles['navbar-list']}>
 
-                            <Link className={navbarStyles['navbar-a']} to="/">
-                                Home
-                            </Link>
-                        </li>
+                        <NavLink className={navbarStyles['navbar-a']} to="/">
+                            Home
+                        </NavLink>
+                    </li>
 
-                        <li className={navbarStyles['navbar-list']}>
-                            <Link className={navbarStyles['navbar-a']} to="/events">
-                                Events
-                            </Link>
-                        </li>
+                    <li className={navbarStyles['navbar-list']}>
+                        <NavLink className={navbarStyles['navbar-a']} to="/events">
+                            Events
+                        </NavLink>
+                    </li>
 
-                        <li className={navbarStyles['navbar-list']}>
-                           <Link className={navbarStyles['navbar-a']} to="/tickets">
-                                Tickets
-                            </Link>
-                        </li>
+                    <li className={navbarStyles['navbar-list']}>
+                        <NavLink className={navbarStyles['navbar-a']} to="/tickets">
+                            Tickets
+                        </NavLink>
+                    </li>
 
-                        <li className={navbarStyles['navbar-list']}>
-                            <Link className={navbarStyles['navbar-a']} to="/about">
-                                About Us
-                            </Link>
-                        </li>
-                    </ul>
+                    <li className={navbarStyles['navbar-list']}>
+                        <NavLink className={navbarStyles['navbar-a']} to="/about">
+                            About Us
+                        </NavLink>
+                    </li>
+                </ul>
+            </nav>
 
-                </nav>
 
-                <button className={navbarStyles['login-link']} onClick={handleConnectWallet}>
-                    <FontAwesomeIcon className={navbarStyles['login-image']} icon={faCircleUser} />
-                    <p className={navbarStyles['login-text']}>Connect Wallet</p>
-                </button>
+            <button className={navbarStyles['login-button']} onClick={handleConnectWallet}>
+                <FontAwesomeIcon className={navbarStyles['login-image']} icon={faCircleUser} />
+            </button>
 
-            </div>
         </header>
     );
 }
