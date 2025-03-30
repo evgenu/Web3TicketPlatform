889957aaa7b47939718372256352098832ef8a38
Georgi Stoev <georgi.stoev.2003@gmail.com>
2025-03-30 20:42:02 +0300

    Enhance DisplayEvent component with error handling and user notifications for contract loading and ticket purchasing

--- app/ticketplatform/src/pages/DisplayEvent.tsx
+++ app/ticketplatform/src/pages/DisplayEvent.tsx
@@ -3,6 +3,7 @@ import { useContract } from "../hooks/contractHook";
 import { useEffect, useState } from "react";
 import displayEventStyles from "../styles/DisplayEvent.module.css";
 import { ethers } from "ethers";
+import { toast } from "react-toastify";
 
 interface Event {
     name: string;
@@ -21,12 +22,18 @@ const DisplayEvent = () => {
 
     useEffect(() => {
         const loadEvents = async () => {
-            if (!contract?.contract) {
-                alert('Contract not loaded');
-                return;
-            };
-            const eventDetails = await contract.contract.getEventDetails(Number(id)) as Event;
-            setEvent(eventDetails);
+            try {
+                if (!contract?.contract) {
+                    toast.error('Contract not loaded', { toastId: "contract-not-loaded" }); 
+                    return;
+                }
+
+                const eventDetails = await contract.contract.getEventDetails(Number(id)) as Event;
+                setEvent(eventDetails);
+            } catch (error) {
+                console.error("Error loading event details:", error);
+                toast.error("Failed to load event details. Please try again later.", { toastId: "failed-loading-events" });
+            }
         };
 
         loadEvents();
@@ -34,13 +41,51 @@ const DisplayEvent = () => {
     }, [contract, id])
 
     const buyTicket = async () => {
-        if (!contract?.contract) {
-            alert('Contract not loaded');
-            return;
-        };
-        const tx = await contract.contract.buyTicket(Number(id), { value: event?.ticketPrice }); {
+        try {
+            if (!contract?.contract) {
+                toast.error('Contract not loaded', { toastId: "contract-not-loaded" });
+                return;
+            }
+
+            if (!event) {
+                toast.error("Event not found", { toastId: "event-not-loaded" });
+                return;
+            }
+
+            if (event.ticketSold >= event.ticketCount) {
+                toast.error("No tickets available", { toastId: "no-tickets-available" });
+                return;
+            }
+
+            if (event.date < Math.floor(Date.now() / 1000)) {
+                toast.error("Event has already passed", { toastId: "overdue-event" });
+                return;
+            }
+
+            const ticketPrice = ethers.formatEther(event.ticketPrice);
+
+            const provider = new ethers.BrowserProvider((window as any).ethereum);
+            const signer = await provider.getSigner();
+
+            const balance = await provider.getBalance(await signer.getAddress());
+
+            if (BigInt(balance) < BigInt(event.ticketPrice)) {
+                toast.error("Insufficient funds", { toastId: "insufficient-funds" });
+                return;
+            }
+
+            const tx = await contract.contract.buyTicket(Number(id), { value: event.ticketPrice });
             console.log(tx);
-        };
+            toast.success("Ticket purchased successfully!", { toastId: "ticket-bought" });
+        } catch (error: any) {
+
+            if (error.action === "sendTransaction" && error.reason === "rejected") {
+                toast.info("Transaction cancelled by user", { toastId: "cancelled-transaction" });
+            } else {
+                console.error("Error purchasing ticket:", error);
+                toast.error("Failed to purchase ticket. Please try again later.", { toastId: "error-buy-ticket" });
+            }
+        }
     }
 
     return (
